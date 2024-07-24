8b311d5c0c04427ba3e77cb35b5b861c2818ebd5
MateusReisdorfer18 <mateusreisdorfer18@gmail.com>
2024-07-24 17:38:39 -0300

    feat service(Sala)

---
+++ cinematic/src/main/java/com/tcc/cinematic/service/SalaService.java
@@ -0,0 +1,49 @@
+package com.tcc.cinematic.service;
+
+import com.tcc.cinematic.DTO.SalaRecordDTO;
+import com.tcc.cinematic.entity.Sala;
+import com.tcc.cinematic.repository.SalaRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class SalaService {
+    @Autowired
+    private SalaRepository repository;
+
+    public List<Sala> findAll() {
+        return this.repository.findAll();
+    }
+
+    public Sala findById(UUID id) {
+        return this.repository.findById(id).orElse(null);
+    }
+
+    public Sala create(SalaRecordDTO salaRecordDTO) {
+        var sala = new Sala();
+        BeanUtils.copyProperties(salaRecordDTO, sala);
+        return this.repository.save(sala);
+    }
+
+    public Sala update(SalaRecordDTO salaRecordDTO) {
+        var salaFound = this.findById(salaRecordDTO.id());
+        if(salaFound == null)
+            return null;
+
+        BeanUtils.copyProperties(salaRecordDTO, salaFound);
+        return this.repository.save(salaFound);
+    }
+
+    public Boolean delete(UUID id) {
+        var salaFound = this.findById(id);
+        if(salaFound == null)
+            return false;
+
+        this.repository.delete(salaFound);
+        return true;
+    }
+}
