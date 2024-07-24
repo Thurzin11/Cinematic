b2ac28ab8522e2a9b2d5619ef96f5cb6df32b20b
MateusReisdorfer18 <mateusreisdorfer18@gmail.com>
2024-07-24 12:55:05 -0300

    feat implementação da api do IBGE

---
+++ cinematic/src/main/java/com/tcc/cinematic/config/IBGEConfiguration.java
@@ -0,0 +1,18 @@
+package com.tcc.cinematic.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@Getter
+public class IBGEConfiguration {
+    @Value("${ibge.base-url}")
+    private String urlBase;
+    @Value("${ibge.estados}")
+    private String urlEstados;
+    @Value("${ibge.cidades")
+    private String urlCidades;
+}
