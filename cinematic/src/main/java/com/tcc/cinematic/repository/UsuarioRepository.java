fc199d7c2c30b0f330915c4e1870ecd832117cee
Arthur <arthursantosgoncalves587@gmail.com>
2024-08-01 17:27:14 -0300

    feat UsuarioRepository(implementação funcao findByNomeAndTipoUsuario)

--- cinematic/src/main/java/com/tcc/cinematic/repository/UsuarioRepository.java
+++ cinematic/src/main/java/com/tcc/cinematic/repository/UsuarioRepository.java
@@ -1,11 +1,14 @@
 package com.tcc.cinematic.repository;
 
 import com.tcc.cinematic.entity.Usuario;
+import com.tcc.cinematic.enums.TipoUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    List<Usuario> findByNomeAndTipoUsuario(String nome, TipoUsuario tipoUsuario);
 }
