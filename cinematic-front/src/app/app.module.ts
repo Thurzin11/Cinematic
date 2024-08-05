fb68f400f2bcae257919fd2c7c41316ddbe3d856
MateusReisdorfer18 <mateusreisdorfer18@gmail.com>
2024-08-05 13:09:15 -0300

    att: menu-sistema imports

--- cinematic-front/src/app/app.module.ts
+++ cinematic-front/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuSistemaComponent } from './components/menu-sistema/menu-sistema.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuSistemaComponent
   ],
   imports: [
     BrowserModule,
