8ea0b82d1ffa5dbaa6d88d7983150a202bd6b900
MateusReisdorfer18 <mateusreisdorfer18@gmail.com>
2024-08-05 13:11:01 -0300

    feat: componente menu-sistema

---
+++ cinematic-front/src/app/components/menu-sistema/menu-sistema.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-sistema',
+  templateUrl: './menu-sistema.component.html',
+  styleUrl: './menu-sistema.component.scss'
+})
+export class MenuSistemaComponent {
+
+}
