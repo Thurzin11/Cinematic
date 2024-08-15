777f755667ac22553cf546f0de1b6c9b8f514501
MateusReisdorfer18 <mateusreisdorfer18@gmail.com>
2024-08-15 11:57:54 -0300

    feat: new routes

--- cinematic-front/src/app/app-routing.module.ts
+++ cinematic-front/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeSistemaComponent } from './components/home-sistema/home-sistema.component';
+import { LoginFuncionarioComponent } from './components/login-funcionario/login-funcionario.component';
 
 const routes: Routes = [
-  {path: "sistema/home", component: HomeSistemaComponent}
+  {path: "sistema/home", component: HomeSistemaComponent},
+  {path: "sistema/login", component: LoginFuncionarioComponent},
+  {path: "", redirectTo: "sistema/login", pathMatch: 'full'}
 ];
 
 @NgModule({
