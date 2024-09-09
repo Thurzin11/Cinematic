1e9fc7c8e2b0baad07b9662c7a94f5c84416fafe
MateusReisdorfer18 <mateusreisdorfer18@gmail.com>
2024-09-09 09:44:47 -0300

    att

--- cinematic/src/main/java/com/tcc/cinematic/repository/SalaRepository.java
+++ cinematic/src/main/java/com/tcc/cinematic/repository/SalaRepository.java
@@ -2,10 +2,17 @@ package com.tcc.cinematic.repository;
 
 import com.tcc.cinematic.entity.Sala;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface SalaRepository extends JpaRepository<Sala, UUID> {
+    @Query("UPDATE Sala s SET s.disponibilidade = false WHERE s.id = :ID")
+    void inativarSala(@Param("ID") UUID id);
+
+    @Query("UPDATE Sala s SET s.disponibilidade = true WHERE s.id = :ID")
+    void ativarSala(@Param("ID") UUID id);
 }
