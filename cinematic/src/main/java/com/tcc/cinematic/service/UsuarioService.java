0ca52f6a0cee141b34b342f34ce397380bc7713f
MateusReisdorfer18 <mateusreisdorfer18@gmail.com>
2024-09-23 13:08:25 -0300

    att

--- cinematic/src/main/java/com/tcc/cinematic/service/UsuarioService.java
+++ cinematic/src/main/java/com/tcc/cinematic/service/UsuarioService.java
@@ -42,7 +42,7 @@ public class UsuarioService {
 
     public Usuario createFuncionario(UsuarioResponseDTO usuarioResponseDTO){
         System.out.println(usuarioResponseDTO.tipoUsuario());
-        var user  = Usuario.builder().tipoUsuario(setTipoUsuario(usuarioResponseDTO.tipoUsuario())).login(usuarioResponseDTO.email()).senha(usuarioResponseDTO.email()).build();
+        var user  = Usuario.builder().tipoUsuario(this.setTipoUsuario(usuarioResponseDTO.tipoUsuario())).login(usuarioResponseDTO.email()).senha(usuarioResponseDTO.email()).build();
         BeanUtils.copyProperties(usuarioResponseDTO,user);
         this.repository.save(user);
         return user;
